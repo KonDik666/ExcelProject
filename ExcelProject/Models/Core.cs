8fa72682a3b7d34006ceb9df4b56719b073b3aea
KonDik666 <system518err@gmail.com>
2023-01-13 15:06:53 +0500

    Добавьте файлы проекта.

---
+++ ExcelProject/Models/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelProject.Models
+{
+    class Core
+    {
+        public PaymentsEntities context = new PaymentsEntities();
+    }
+}
